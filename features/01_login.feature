b5c3a3f7dffe2d5b604a9c3820181f4d67913543
sulakshana-763 <sulakshana763@gmail.com>
2025-03-10 11:51:43 -0700

    program page added

---
+++ features/01_login.feature
@@ -0,0 +1,6 @@
+Feature: verify the login 
+Scenario: login with valid credentials
+Given the url of the application
+When the url is clicked
+Then the browser opens
+
--- pageObject/POManager.js
+++ pageObject/POManager.js
@@ -1,4 +1,5 @@
 const {LoginPage} = require('./LoginPage');
+const {ProgramPage} = require('./ProgramPage');
 
 
 class POManager{
@@ -6,10 +7,15 @@ class POManager{
     constructor(page){       
         this.page = page;
         this.loginPage = new LoginPage(this.page);
+        this.programPage = new ProgramPage(this.page);
     }
 
     getLoginPage(){
         return this.loginPage;
+    }
+
+    getProgramPage(){
+        return this.programPage;
     }
 }
 module.exports = {POManager};
