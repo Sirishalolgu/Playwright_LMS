0b3fd7db9671dd2242b2b60c10993b3bf3e88662
RuchitaKesarkar1 <kesarkar.ruchita3@gmail.com>
2025-03-26 15:37:51 -0700

    POMmanager

--- pageObject/POManger.js
+++ pageObject/POManger.js
@@ -1,22 +1,21 @@
 const LoginPage = require('./LoginPage');
 const ProgramPage = require('./ProgramPage');
-const ClassPage = require('./ClassPage');
+
 
 class POManager{
+
     constructor(page){       
         this.page = page;
         this.loginPage = new LoginPage(this.page);
         this.programPage = new ProgramPage(this.page);
-        this.classPage = new ClassPage(this.page);
     }
+
     getLoginPage(){
         return this.loginPage;
     }
+
     getProgramPage(){
         return this.programPage;
-    }
-    getClassPage(){
-        return this.classPage;
     }
 }
 module.exports = POManager;
