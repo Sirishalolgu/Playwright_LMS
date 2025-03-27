9b6f6837b8ec76c2fe5bbdfa83cf90192d4e5f20
RuchitaKesarkar1 <kesarkar.ruchita3@gmail.com>
2025-03-26 17:02:56 -0700

    LogOut added

---
+++ pageObject/LogoutPage.js
@@ -0,0 +1,12 @@
+class LogoutPage {
+    constructor(page) {
+      this.page = page;
+      this.logoutButton = page.locator('//button[@id="logout"]');
+    }
+    async clickLogout() {
+      await this.logoutButton.click();
+    }
+    
+  }
+  
+  module.exports = LogoutPage;
